97334c4b453170850c4c75e70dba70a3a1e0b62b
tayqiter00 <tayqiter00@gmail.com>
2024-07-14 09:03:53 +0800

    added start screen

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 pygame.display.set_caption("My Flappy Bird")
 
+START_SCREEN = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'message.png')), (WIDTH, HEIGHT))
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'background-day.png')), (WIDTH, HEIGHT))
 BASE = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'base.png')), (WIDTH, HEIGHT / 10))
 
@@ -82,7 +83,7 @@ def shifted_pipes(pipes_btm):
 def main():
     clock = pygame.time.Clock()
     run = True
-    game_start = True
+    game_start = False
 
     velocity = 0
     gravity = 0.5
@@ -96,35 +97,47 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
+                pygame.quit() 
             
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     run = False
                     pygame.quit()
-            
+                
                 if event.key == pygame.K_SPACE:
+                    game_start = True
                     velocity = jump
 
             if event.type == pygame.MOUSEBUTTONDOWN:
+                game_start = True
                 velocity = jump
             
             if event.type == PIPE_TIMER:
                 pipes_btm.append(PIPE_BTM.get_rect(topleft=(432, randint(350, 450))))
                 pipes_top = shifted_pipes(pipes_btm)
-    
-        velocity += gravity
-        red_bird_rect.y += velocity
 
-        if red_bird_rect.y < 0:
-            red_bird_rect.y = 0
-        if red_bird_rect.colliderect(base_rect):
-            run = False
+        if  game_start == False:
+            pipes_top = []
+            pipes_btm = []
+            red_bird_rect = RED_BIRD_SURF.get_rect(midleft=(32, HEIGHT / 2))
+            WIN.blit(BACKGROUND, (0, 0))
+            WIN.blit(BASE, (0, 700))
+            WIN.blit(START_SCREEN, (0, 0))
+            pygame.display.update()
 
-        pipes_btm, pipes_top = pipe_move(pipes_btm, pipes_top)
-        bird_animation()
-        draw_window(red_bird_rect, pipes_btm, pipes_top)
-        run = collisions(red_bird_rect, pipes_btm, pipes_top, base_rect)
+        else:
+            velocity += gravity
+            red_bird_rect.y += velocity
+
+            if red_bird_rect.y < 0:
+                red_bird_rect.y = 0
+            if red_bird_rect.colliderect(base_rect):
+                game_start = False
+
+            pipes_btm, pipes_top = pipe_move(pipes_btm, pipes_top)
+            bird_animation()
+            draw_window(red_bird_rect, pipes_btm, pipes_top)
+            game_start = collisions(red_bird_rect, pipes_btm, pipes_top, base_rect)
 
 if __name__ == "__main__":
     main()
