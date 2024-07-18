0fda353451153c7c2484f92c2566a2df4f95c949
tayqiter00 <tayqiter00@gmail.com>
2024-07-18 18:27:19 +0800

    added score and game over screen

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 pygame.display.set_caption("My Flappy Bird")
 
+FONT = pygame.font.Font('font/Pixeltype.ttf', 50)
 START_SCREEN = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'message.png')), (WIDTH, HEIGHT))
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'background-day.png')), (WIDTH, HEIGHT))
 BASE = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'base.png')), (WIDTH, HEIGHT / 10))
+GAME_OVER = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'gameover.png')), (WIDTH, HEIGHT / 10))
 
 RED_BIRD_DOWNFLAP = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'redbird-downflap.png')), (WIDTH / 7, HEIGHT / 15))
 RED_BIRD_MIDFLAP = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'sprites', 'redbird-midflap.png')), (WIDTH / 7, HEIGHT / 15))
@@ -68,7 +70,7 @@ def pipe_move(pipes_btm, pipes_top):
         pipes_top = [pipe for pipe in pipes_top if pipe.x > -100]
     return pipes_btm, pipes_top
 
-def draw_window(red, pipes_btm, pipes_top, bases):
+def draw_window(red, pipes_btm, pipes_top, bases, score):
     WIN.blit(BACKGROUND, (0, 0))
 
     for pipe_rect in pipes_btm:
@@ -79,12 +81,8 @@ def draw_window(red, pipes_btm, pipes_top, bases):
         WIN.blit(BASE, base_rect)
 
     WIN.blit(RED_BIRD_SURF, (red.x, red.y))
-    if pipes_btm:
-        if pipes_btm[0].x <= red.x:
-            WIN.blit(ZERO, ZERO_RECT)
+    return (display_score(score, pipes_btm, red))    
     
-    pygame.display.update()
-
 def shifted_pipes(pipes_btm):
     pipes_top = []
     for pipe_rect in pipes_btm:
@@ -93,11 +91,22 @@ def shifted_pipes(pipes_btm):
         pipes_top.append(shifted_rect)
     return pipes_top
 
+def display_score(score, pipes_btm, red):
+    if pipes_btm:
+        if pipes_btm[0].x == red.x + 1:
+            score += 1
+    score_surf = FONT.render(f'Score: {score}', False, (255, 255, 255))
+    WIN.blit(score_surf, (0, 0))
+    pygame.display.update()
+    return (score)
+
 def main():
     clock = pygame.time.Clock()
     run = True
+    first_run = True
     game_start = False
- 
+
+    score = 0
     velocity = 0
     gravity = 0.5
     jump = -10
@@ -121,26 +130,40 @@ def main():
                 
                 if event.key == pygame.K_SPACE:
                     game_start = True
+                    first_run = False
                     velocity = jump
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 game_start = True
+                first_run = False
                 velocity = jump
             
             if event.type == PIPE_TIMER:
                 pipes_btm.append(PIPE_BTM.get_rect(topleft=(432, randint(350, 450))))
                 pipes_top = shifted_pipes(pipes_btm)
 
-        if  game_start == False:
-            pipes_top = [] 
-            pipes_btm = []
-            red_bird_rect = RED_BIRD_SURF.get_rect(midleft=(32, HEIGHT / 2))
-            WIN.blit(BACKGROUND, (0, 0))
-            bases = base_move(bases)
-            for base_rect in bases:
-                WIN.blit(BASE, base_rect)
-            WIN.blit(START_SCREEN, (0, 0))
-            pygame.display.update()
+        if game_start == False:
+            if first_run:
+                red_bird_rect = RED_BIRD_SURF.get_rect(midleft=(32, HEIGHT / 2))
+                WIN.blit(BACKGROUND, (0, 0))
+                bases = base_move(bases)
+                for base_rect in bases:
+                    WIN.blit(BASE, base_rect)
+                WIN.blit(START_SCREEN, (0, 0))
+                pygame.display.update()
+            else:
+                if score != 0:
+                    old_score = score
+                score = 0
+                pipes_top = [] 
+                pipes_btm = []
+                red_bird_rect = RED_BIRD_SURF.get_rect(midleft=(32, HEIGHT / 2))
+                WIN.blit(BACKGROUND, (0, 0))
+                WIN.blit(BASE, (0, 700))
+                WIN.blit(GAME_OVER, (0, HEIGHT / 2 - 80))
+                score_surf = FONT.render(f'Score: {old_score}', False, (255, 255, 255))
+                WIN.blit(score_surf, (0, 0))
+                pygame.display.update()
 
         else:
             bases = base_move(bases)
@@ -152,7 +175,8 @@ def main():
 
             pipes_btm, pipes_top = pipe_move(pipes_btm, pipes_top)
             bird_animation()
-            draw_window(red_bird_rect, pipes_btm, pipes_top, bases)
+            score = draw_window(red_bird_rect, pipes_btm, pipes_top, bases, score)
+            pygame.display.update()
             game_start = collisions(red_bird_rect, pipes_btm, pipes_top, bases)
 
 if __name__ == "__main__":
